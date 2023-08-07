59a88d0fae0e3220cb428ef1bbe657c2437d49b2
Hitesh Choudhary <hchoudhary@zetaglobal.com>
2023-08-07 14:59:13 +0530

    Add project files.

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products.FirstOrDefault(p => p.Id == id));
+        }
+
+    }
+}
---
+++ WebApi/Entities/Product.cs
@@ -0,0 +1,82 @@
+﻿namespace WebApi.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.ProductId = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the product identifier.
+        /// </summary>
+        /// <value>
+        /// The product identifier.
+        /// </value>
+        public string ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public long Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the picture URL.
+        /// </summary>
+        /// <value>
+        /// The picture URL.
+        /// </value>
+        public string PictureUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public string Type { get; set;}
+
+        /// <summary>
+        /// Gets or sets the brand.
+        /// </summary>
+        /// <value>
+        /// The brand.
+        /// </value>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity in stock.
+        /// </summary>
+        /// <value>
+        /// The quantity in stock.
+        /// </value>
+        public int QuantityInStock { get; set; }
+    }
+}
